865d2f19ef449334d59a40cdf6adeef9cb208010
David Strachan <davidstrachan92@gmail.com>
2025-01-25 14:05:46 +0000

    Add LiquidContainer

---
+++ Assets/Scripts/LiquidContainer.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class LiquidContainer : MonoBehaviour
+{
+    public float Volume { get; private set; }
+
+    public float FillAmount
+    {
+        get => fillAmount;
+        set => fillAmount = Mathf.Clamp01(value);
+    }
+
+    [SerializeField] private MeshRenderer liquidRenderer;
+    [SerializeField] private Material liquidMaterial;
+    [SerializeField] [Range(0, 1)] private float fillAmount = 0.5f;
+
+    private static readonly int GravityDirectionId = Shader.PropertyToID("_GravityDirection");
+    private static readonly int SurfaceLevelId = Shader.PropertyToID("_SurfaceLevel");
+
+    private void Awake()
+    {
+        Debug.Assert(liquidRenderer is not null);
+        Debug.Assert(liquidMaterial is not null);
+
+        var meshFilter = liquidRenderer.GetComponent<MeshFilter>();
+        var boundsSize = meshFilter.sharedMesh.bounds.size;
+        var scale = transform.lossyScale;
+        Volume = boundsSize.x * boundsSize.y * boundsSize.z * scale.x * scale.y * scale.y * 1000;
+
+        liquidRenderer.sharedMaterial = liquidMaterial;
+
+        liquidMaterial.SetVector(GravityDirectionId, Vector3.down);
+    }
+
+    private void Update()
+    {
+        UpdateSurface();
+    }
+
+    private void UpdateSurface()
+    {
+        if (fillAmount > 0)
+        {
+            var bounds = liquidRenderer.bounds;
+            var min = bounds.min.y;
+            var max = bounds.max.y;
+
+            var surfaceLevel = bounds.center;
+            var sin = Mathf.Sin(2 * Mathf.PI * Time.time);
+            surfaceLevel.x *= sin;
+            surfaceLevel.y = fillAmount * (max - min) + min;
+            surfaceLevel.z *= sin;
+            liquidRenderer.material.SetVector(SurfaceLevelId, surfaceLevel);
+
+            liquidRenderer.enabled = true;
+        }
+        else
+        {
+            liquidRenderer.enabled = false;
+        }
+    }
+}
