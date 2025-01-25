8b9afd70c388b751e56be3961085a4be22927991
David Strachan <davidstrachan92@gmail.com>
2025-01-25 22:03:35 +0000

    Transfer liquid between containers

--- Assets/Scripts/LiquidContainer.cs
+++ Assets/Scripts/LiquidContainer.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 public class LiquidContainer : MonoBehaviour
@@ -6,9 +7,10 @@ public class LiquidContainer : MonoBehaviour
 
     [SerializeField] private MeshRenderer liquidRenderer;
     [SerializeField] private Material liquidMaterial;
-    [SerializeField] private MeshRenderer bottleneck;
+    [SerializeField] private Collider bottleneckCollider;
     [SerializeField] [Range(0, 1)] private float fillAmount = 0.5f;
     [SerializeField] private float pourSpeed = 1;
+    [SerializeField] private float splashRadius = 0.025f;
 
     private static readonly int GravityDirectionId = Shader.PropertyToID("_GravityDirection");
     private static readonly int SurfaceLevelId = Shader.PropertyToID("_SurfaceLevel");
@@ -58,17 +60,47 @@ public class LiquidContainer : MonoBehaviour
         return surfaceLevel;
     }
 
-    private bool ShouldPour(float surfaceLevel) => surfaceLevel > bottleneck.bounds.min.y ||
+    private bool ShouldPour(float surfaceLevel) => surfaceLevel > bottleneckCollider.bounds.min.y ||
                                                    transform.rotation.eulerAngles.z is > 100 and < 260;
 
     private void PourLiquid()
     {
-        var liquidToRemove = pourSpeed * Time.deltaTime * 0.25f;
-        fillAmount = Mathf.Max(0, fillAmount - liquidToRemove);
+        var percentageToRemove = pourSpeed * Time.deltaTime * 0.25f;
+        fillAmount = Mathf.Max(0, fillAmount - percentageToRemove);
 
-        // TODO: Transfer liquid
+        // TODO: Where is the point that we hit with the liquid? For now we just assume the cup is below the bottleneck.
+        if (TryGetContainer(bottleneckCollider.bounds.min, out var container))
+        {
+            Debug.Log($"{gameObject.name} filling {container.gameObject.name}");
+            // TODO: Did we actually hit the bottleneck?
+            var liquidAmount = Volume * percentageToRemove;
+            // TODO: FIXME
+            container.fillAmount = Mathf.Clamp01(container.fillAmount + liquidAmount);
+        }
 
         // TODO: Particles
+    }
+
+    private bool TryGetContainer(Vector3 origin, out LiquidContainer liquidContainer)
+    {
+        var ray = new Ray(origin, Vector3.down);
+
+        // var raycastHits = new RaycastHit[10]; // TODO: how many hits do we want?
+        var raycastHits = Physics.SphereCastAll(ray, splashRadius);
+        foreach (var hit in raycastHits.OrderBy(x => x.distance))
+        {
+            if (hit.collider is null || hit.collider == bottleneckCollider) continue;
+
+            var container = hit.collider.GetComponentInParent<LiquidContainer>();
+            if (container is not null)
+            {
+                liquidContainer = container;
+                return true;
+            }
+        }
+
+        liquidContainer = null;
+        return false;
     }
 
     private void OnDrawGizmos()
@@ -80,5 +112,11 @@ public class LiquidContainer : MonoBehaviour
         var center = liquidRenderer.bounds.center;
         center.y = surfaceLevel.y;
         Gizmos.DrawSphere(center, 0.01f);
+
+        if (TryGetContainer(bottleneckCollider.bounds.min, out var container))
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(container.bottleneckCollider.bounds.center, 0.01f);
+        }
     }
 }
