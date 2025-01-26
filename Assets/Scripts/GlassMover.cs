84301c6172ff394fcfe5716f964f76379083e1a9
Michael <michael@makarness.com>
2025-01-26 16:17:56 +0000

    glass spawning

---
+++ Assets/Scripts/GlassMover.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GlassMover : MonoBehaviour
+    {
+        public Vector3 target;
+        public Vector3 startPos;
+        public float glassSpeed;
+
+        private float _timeSinceStarted;
+
+        public bool started;
+        public AnimationCurve moveCurve;
+
+        private void Update()
+        {
+            if (started)
+            {
+
+                _timeSinceStarted += Time.deltaTime;
+                float t1 = moveCurve.Evaluate(_timeSinceStarted * glassSpeed);
+                transform.localPosition = Vector3.Lerp(startPos, target, t1);
+
+                // If the object has arrived, stop the coroutine
+                if (Vector3.Distance(transform.position, target) < 0.01f)
+                {
+                    started = false;
+                }
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/GlassSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace DefaultNamespace
+{
+    public class GlassSpawner : MonoBehaviour
+    {
+        public GameObject glass;
+        public Transform spawnPos;
+
+        public Transform startPoint;
+        public Transform endPoint;
+        public AnimationCurve moveCurve;
+
+        public float howOftenAGlassAppearsMin = 10f;
+        public float howOftenAGlassAppearsMax = 2f;
+        public float howOftenAGlassAppears = 2f;
+        public float glassSpeed = 1.4f;
+        public float _lastGlass;
+
+        public bool activeGlass = false;
+
+        private void Start()
+        {
+            howOftenAGlassAppears = Random.Range(howOftenAGlassAppearsMin, howOftenAGlassAppearsMax);
+        }
+
+        private void Update()
+        {
+            if (!activeGlass && _lastGlass + howOftenAGlassAppears < Time.time)
+            {
+                var spawnedGlass = Instantiate(glass, transform);
+                spawnedGlass.transform.localPosition = spawnPos.localPosition;
+                _lastGlass = Time.time;
+
+                var target = new Vector3(Random.Range(startPoint.localPosition.x, endPoint.localPosition.x),0,0);
+
+                var glassMover = spawnedGlass.AddComponent<GlassMover>();
+                glassMover.target = target;
+                glassMover.glassSpeed = glassSpeed;
+                glassMover.moveCurve = moveCurve;
+                glassMover.startPos = spawnedGlass.transform.localPosition;
+                glassMover.started = true;
+
+                howOftenAGlassAppears = Random.Range(howOftenAGlassAppearsMin, howOftenAGlassAppearsMax);
+                activeGlass = true;
+            }
+
+        }
+    }
+}
